7f1ab1c782c63042d5094ad1aa22cc46344c70f8
AnaWhibley <anawhibley@gmail.com>
2020-01-07 15:02:12 +0000

    Minor changes

--- src/watch/presenter/Main.java
+++ src/watch/presenter/Main.java
@@ -1,13 +1,8 @@
 package watch.presenter;
 
-import watch.model.Watch;
-import watch.view.SwingWatchDisplay;
-
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 
 public class Main extends JFrame {
-    private SwingWatchDisplay display;
 
     public static void main(String[] args) {
         new MainFrame().execute();
