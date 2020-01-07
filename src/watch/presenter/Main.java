3c5a1ddb7c969a6ce23b94bd8ee871769058d335
AnaWhibley <anawhibley@gmail.com>
2020-01-07 15:03:04 +0000

    Minor changes

--- src/watch/presenter/Main.java
+++ src/watch/presenter/Main.java
@@ -1,10 +1,9 @@
 package watch.presenter;
 
-import javax.swing.JFrame;
-
-public class Main extends JFrame {
+public class Main{
 
     public static void main(String[] args) {
+        System.out.println("Watch working ...");
         new MainFrame().execute();
     }
 
